b33f82c9044bd3c13993607bfb743053952f1f57
Rownok Jahan <rownok.cse.20220104151@aust.edu>
2024-08-30 18:47:52 +0600

    modifications

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -25,10 +25,10 @@ const Sidebar = () => {
           </div>
         </div>
         <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4">
-          <h1>Create Your First PlayList</h1>
-          <p className="font-light">It's easy we will help you</p>
+          <h1>Create Your first Playlist</h1>
+          <p className="font-light">It's easy, we'll help you</p>
           <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">
-            Create PlayList
+            Create Playlist
           </button>
         </div>
         <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4">
