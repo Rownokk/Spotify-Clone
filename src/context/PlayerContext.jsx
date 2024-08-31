9d4b81ac799846a265ee301785e26822428e20e0
Rownokk <mowmita878@gmail.com>
2024-08-31 13:20:29 +0600

    frontend ✔️

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -37,6 +37,30 @@ const PlayerContextProvider = (props) => {
       setPlayStatus(true);
   }
 
+const previous = async () => {
+    if(track.id>0){
+      await setTrack(songsData[track.id-1]);
+      await audioRef.current.play();
+      setPlayStatus(true);
+    }
+
+}
+
+const next = async () => {
+  if(track.id < songsData.length-1){
+    await setTrack(songsData[track.id+1]);
+    await audioRef.current.play();
+    setPlayStatus(true);
+  }
+
+}
+const seekSong = async (e) => {
+     audioRef.current.currentTime = ((e.nativeEvent.offsetX / seekBg.current.offsetWidth )* audioRef.current.duration);
+
+}
+
+
+
 useEffect(()=>{
       setTimeout(()=>{
         audioRef.current.ontimeupdate = () => {
@@ -64,6 +88,8 @@ useEffect(()=>{
     time,setTime,
     play,pause,
     playWithId,
+    previous,next,
+    seekSong,
   };
   return (
     <PlayerContext.Provider value={contextValue}>
